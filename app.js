c3ffae7800241976113402f1ec76aeb63d9b26d0
Ann Vershkova <aniamanson@gmail.com>
2021-04-04 12:18:02 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const App = {
       title: "Список покупок",
       inputValue: '',
       notes: [],
+      buyLast: [],
     };
   },
   methods: {
@@ -19,6 +20,12 @@ const App = {
     },
     removeNote(idx) {
       this.notes.splice(idx, 1)
+    },
+    doneNote(idx) {
+      this.buyLast.push(this.notes.splice(idx, 1)[0])
+    },
+    returnNote(idx) {
+      this.notes.push(this.buyLast.splice(idx, 1)[0])
     }
   }
 };
